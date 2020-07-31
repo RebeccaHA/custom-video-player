85c0366892f45d4b76fff68bee768ee50120dc5a
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-31 15:08:56 +0100

    added event listeners for swap and updated rate value

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const currencyElementTwo = document.getElementById("currency-two");
 const amountElementTwo = document.getElementById("amount-two");
 const amountElementOne = document.getElementById("amount-one");
 
-const rate = document.getElementById("rate");
+const rates = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 currencyElementOne.addEventListener("change", calculate);
 amountElementOne.addEventListener("input", calculate);
 currencyElementTwo.addEventListener("change", calculate);
 amountElementTwo.addEventListener("input", calculate);
+swap.addEventListener("click", () => {
+  const temp = currencyElementOne.value;
+  currencyElementOne.value = currencyElementTwo.value;
+  currentElementTwo.value = temp;
+  calculate();
+});
 
 function calculate() {
   const currencyOne = currencyElementOne.value;
   const currencyTwo = currencyElementTwo.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currencyTwo];
+
+      rates.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+      amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
+    });
 }
 
 calculate();
