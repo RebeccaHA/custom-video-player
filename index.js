8b46c02fdb90ee5b1c161e0878d4ad871ecd086e
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-31 12:02:20 +0100

    added event listeners

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function calculate() {
-  fetch("items.json")
-  .then(response => response.json())
-  .then(data => )
-  ;
-}
+const currencyElementOne = document.getElementById("currency-one");
+const currencyElementTwo = document.getElementById("currency-two");
+const amountElementTwo = document.getElementById("amount-two");
+const amountElementOne = document.getElementById("amount-one");
+
+const rate = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+currencyElementOne.addEventListener("change", calculate);
+amountElementOne.addEventListener("input", calculate);
+currencyElementTwo.addEventListener("change", calculate);
+amountElementTwo.addEventListener("input", calculate);
+
+function calculate() {}
 
 calculate();
