52e4441c2898ba4f4a2cdf5ae80b137036929013
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-31 18:49:13 +0100

    added scroll function, which shows loader then fetches the next set of blogs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById("post-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts() {
@@ -32,11 +32,25 @@ async function showPosts() {
   });
 }
 
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+  }, 1000);
+
+  setTimeout(() => {
+    page++;
+    showPosts();
+  }, 300);
+}
+
 showPosts();
 
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
   }
 });
